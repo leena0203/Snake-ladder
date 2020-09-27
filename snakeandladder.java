d94298ecba1b487badae5611151c00d8559edeaa
leena0203 <leenasarode02@gmail.com>
2020-09-28 01:48:27 +0530

    UC4 committed

--- snakeandladder.java
+++ snakeandladder.java
@@ -3,21 +3,36 @@ public static void main(String[] args) {
    final        int no_play = 0;
 		final int snake = 1;
 		final int ladder = 2;
+                final int max=100;
+                final int min= 0;
 		 
 		int position = 0;
                 
-                
+                while (position != max){
+
 		int roll = (int)Math.floor(Math.random() * 10) % 6 + 1 ;
 		System.out.println("Number on dice : "+roll);
 		int option = (int)Math.floor(Math.random() * 10) % 3;
-                
-		if(option == no_play)
-			System.out.println("Stay on same position");
-		else if(option == ladder)
-                        
-			System.out.println(" Climb ladder, New position: "+roll);
-		else 
-                         
-			System.out.println("Snake, New Position "+roll);
+                switch(option){
+                  case 0:
+                  position = position;
+                  break;
+                  case 1:
+                  if(roll + position <=100) ;
+                      position -= roll;
+                  
+                  break;
+                  case 2:
+                  if(position - roll >=0) ;
+                      position += roll;
+                  
+                   break;
+                  }
+                   if(position < min){
+                    position = min;
+                  }
+                  System.out.println("Current Position: " + position);
+ 
 }
-	}
+}
+}
