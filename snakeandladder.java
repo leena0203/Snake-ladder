fecac0697ffff2543cab8340330de554ad5e3de3
leena0203 <leenasarode02@gmail.com>
2020-09-28 01:58:36 +0530

    UC7 committed

--- snakeandladder.java
+++ snakeandladder.java
@@ -1,45 +1,78 @@
-public class snakeandladder{
-public static void main(String[] args) {
-   final        int no_play = 0;
-		final int snake = 1;
-		final int ladder = 2;
-                final int max=100;
-                final int min= 0;
+public class snakeandladder {
+	
+	//GLOBAL VARIABLES
+	static final int no_play = 0;
+	static final int snake = 1;
+	static final int ladder = 2;
+	static final int max_position = 100;
+	static final int min_position = 0;
+	static int total_dice_roll = 0;
+	
+	//function to find new position
+	public static int dice_throw(int position) {
+		total_dice_roll++;												//counting overall dice rolls
+		int dice_num = (int)Math.floor(Math.random() * 10) % 6 + 1 ;
+		System.out.println("Number on dice : "+dice_num);
+		
+		//checking which option we got
+		int option_check = (int)Math.floor(Math.random() * 10) % 3;
+		
+		switch(option_check) {
+		case snake:
+			System.out.println("You got snake.");
+			position -= dice_num;
+			break;
+		case 2:
+			System.out.println("You got ladder.");
+			position += dice_num;
+			position = dice_throw(position);							//If player gets ladder, plays again
+			break;
+		default:
+			System.out.println("Be on a same position");
+			break;
+		}
+		//reseting position if becomes less than min_position
+		if(position < min_position) {
+			position = min_position;
+		}
+		//if goes above 100 then initial position
+		if(position > max_position) {
+			position -= dice_num;
+		}
+		
+		return position;
+	}	
+	
+	public static void main(String[] args) {
+		//constants
+		int player1_num = 0;
+		int player2_num = 1;
 		 
-		int position = 0;
-                int dice_rolls = 0;
-                while (position != max){
-
-		int roll = (int)Math.floor(Math.random() * 10) % 6 + 1 ;
-		System.out.println("Number on dice : "+roll);
-                dice_rolls++;
-		int option = (int)Math.floor(Math.random() * 10) % 3;
-                switch(option){
-                  case 0:
-                  position = position;
-                  break;
-                  case 1:
-                  if(roll + position <=100) ;
-                      position -= roll;
-                  
-                  break;
-                  case 2:
-                  if(position - roll >=0) ;
-                      position += roll;
-                  
-                   break;
-                  }
-                   if(position < min) {
-				position = min;
+		//variables
+		int player1_position = 0;
+		int player2_position = 0;
+		
+		int roll = (int)Math.floor(Math.random() * 10) % 2;
+		//throwing a dice
+		while(player1_position != max_position && player2_position != max_position) {
+			if(roll%2 == player1_num) {
+				System.out.println("Player 1 playing.....");
+				player1_position = dice_throw(player1_position);
 			}
-			//if goes above 100 then initial position
-			if(position > max) {
-				position -= roll;
-                        }
-                  
-                  System.out.println("Current Position: " + position);
-                }
-      System.out.println("Number of time dice rolls:" +dice_rolls);
-
-}
+			
+			if(roll%2 == player2_num) {
+				System.out.println("Player 2 playing.....");
+				player2_position = dice_throw(player2_position);
+			}
+			
+			System.out.println("Player 1 position : "+player1_position);
+			System.out.println("Player 2 position : "+player2_position);
+			System.out.println();
+			System.out.println();
+			roll++;
+		}
+		if(player1_position == max_position)	System.out.println("PLAYER 1 WON THE GAME");
+		else	System.out.println("PLAYER 2 WON THE GAME");
+		System.out.println("Number of dice rolls required to win the game : "+total_dice_roll);
+	}
 }
