ca5e4d457da5d8ca3bbf6f3be3b7de4e5b9c6f08
leena0203 <leenasarode02@gmail.com>
2020-09-28 01:39:28 +0530

    UC2 committed

--- snakeandladder.java
+++ snakeandladder.java
@@ -1,6 +1,10 @@
 public class snakeandladder{
 public static void main(String[] args) {
-int position=0;
-		System.out.println("Start Position 0");
+   int position=0;
+   int roll; 
+   roll =(int)( Math.floor(Math.random() * 10) % 6);
+   int n = position + roll;
+
+   System.out.println("Number on dice:" + n);
 }
 	}
