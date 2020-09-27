104b5cd0365d9f4d72134601651d99a2d93d8b6a
leena0203 <leenasarode02@gmail.com>
2020-09-28 01:45:00 +0530

    UC3 committed

--- snakeandladder.java
+++ snakeandladder.java
@@ -1,10 +1,23 @@
 public class snakeandladder{
 public static void main(String[] args) {
-   int position=0;
-   int roll; 
-   roll =(int)( Math.floor(Math.random() * 10) % 6);
-   int n = position + roll;
-
-   System.out.println("Number on dice:" + n);
+   final        int no_play = 0;
+		final int snake = 1;
+		final int ladder = 2;
+		 
+		int position = 0;
+                
+                
+		int roll = (int)Math.floor(Math.random() * 10) % 6 + 1 ;
+		System.out.println("Number on dice : "+roll);
+		int option = (int)Math.floor(Math.random() * 10) % 3;
+                
+		if(option == no_play)
+			System.out.println("Stay on same position");
+		else if(option == ladder)
+                        
+			System.out.println(" Climb ladder, New position: "+roll);
+		else 
+                         
+			System.out.println("Snake, New Position "+roll);
 }
 	}
